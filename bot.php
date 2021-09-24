a01c2c6be0831d5554a004dcfb6430284c9d9dcf
Ryan Kurniadi <35515863+ryankrv@users.noreply.github.com>
2021-09-24 19:59:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,25 +1,19 @@
 <?php 
+    include 'config.php';
     $t = "\e[97m";
     $merah = "\e[91m";
-    echo "Ini Adalah Request API \n";
-    //$nama = readline("Masukkan Nama : ");
-    //echo "Namamu adalah " .$nama. "\n";
-
+    echo $merah."BOT Go Novel Bye Ryan \n".$t;
     $urel = 'https://api.igonovel.com/v1/me/info';
 
     function get($url){
-        $uid = '99ED4697C480A58A';
-        $token = 'By0DcAI8XT4NclE_VyZdbwI5CjhXOlUwAm9QOQQkXHNUJwUgUyYAPQpmXT8EZFNiVmEJNABtUj9TNVo4A3w';
-
-
+        global $uid, $token, $timestamp;
         $ch = curl_init();
         //set URL
         curl_setopt($ch, CURLOPT_URL, $url);
         //get Data
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        $timestamp = 'timeStamp=1632486949&apiSign=1B3146C5CA33A5200502639D35CCDC4F';
-        $headers = [
+       $headers = [
             'qr-client: android',
             'qr-uuid: '.$uid,
             'qr-language: 3',
@@ -52,10 +46,9 @@
 
     if($data['status'] == 1){
         
-        echo "Nama Akun: ".$data['data']['nickname'];
+        echo "Nama Akun: ".$data['data']['nickname']."\n";
     }
     else{
-        echo "Status: ".$data['msg'];
+        echo "Status: ".$data['msg']."\n";
     }
-
     ?>
