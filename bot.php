2561a92b26141be3e67c8e065a619b6cfa206590
Ryan Kurniadi <35515863+ryankrv@users.noreply.github.com>
2021-09-25 17:58:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@
     "- Menggunaka Bot Sama Saja Meggunakan Cara Ilegal \n".
     "\n";
     //$timestamp = "timeStamp=1632546383&apiSign=4FC7D956D8E33D81DA3BDB285C370F57";
-    $timestamp = readline($bold.$kuning."Masukkan timeStam & apiSign : ".$normal.$t);
+    $timestamp = readline($bold.$kuning."Masukkan Request Body 1 : ".$normal.$t);
     //Fungsi
     function get($url){
         global $uid, $token, $timestamp;
@@ -133,18 +133,56 @@
         return $response_json;
     }
 
+    function ccs($url){
+        global $uid, $token, $timestamp;
+        $ch = curl_init();
+        //set URL
+        curl_setopt($ch, CURLOPT_URL, $url.$timestamp);
+        //get Data
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+       $headers = [
+            'qr-client: android',
+            'qr-uuid: '.$uid,
+            'qr-language: 3',
+            'qr-timezone: Asia/Jakarta',
+            'qr-token: '.$token,
+            'qr-version: 1.1.6',
+            'qr-sex: 1',
+            'qr-model: POCOPHONE F1',
+            'qr-r: 0',
+            'share-stamp: ',
+            'share-text: ',
+            'Content-Type: application/x-www-form-urlencoded',
+            'Content-Length: 61',
+            'Host: api.igonovel.com',
+            'Connection: Keep-Alive',
+            'Accept-Encoding: gzip',
+            'User-Agent: okhttp/4.9.1'
+        ];
+        
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $response_json = curl_exec($ch);
+        curl_close($ch);
+        return $response_json;
+    }
+
+    
+
     //Requset
     $profil = 'https://api.igonovel.com/v1/me/info';
     $chest = 'https://api.igonovel.com/v112/user/get_mess_chest_num';
     $bchest = 'https://api.igonovel.com/v113/user/get_chest_rewards';
+    $cc= 'https://api.igonovel.com/v112/user/receive_chest?';
+
     //ke server
     $prof = get($profil);
     $getchest = cekchest($chest);
-    $bukces = bukachest($bchest);
+    $ccss = ccs($cc);
     //
     $pr = json_decode($prof, TRUE);
     $chstot =  json_decode($getchest, TRUE);
-    $openchest = json_decode($bukces, TRUE);
+    
+    $claim = json_decode($ccss, TRUE);
 
 
     if($pr['status'] == 1){
@@ -169,24 +207,31 @@
                 {
                     if($chstot['status'] == 1){
                         $bykces = $chstot['data']['chest_num'];
-                        while($bykces != 0){
-                            echo "Membuka Chest Dalam : ";
-                            for($waktu = 5; $waktu > 0; $waktu--){
-                                echo $waktu;
-                                sleep(1);
-                                $pjg = strlen($waktu);
-                                for ($pjg; $pjg > 0; $pjg--){
-                                    echo chr(8);
+                        $rebod = '';
+                        //cek jumlah chest dan siapkan Execute;
+                        if($bykces != 0){
+                            $bukces = bukachest($bchest);
+                            $openchest = json_decode($bukces, TRUE);
+                            while($bykces != 0){
+                                echo "Membuka Chest Dalam : ";
+                                for($waktu = 5; $waktu > 0; $waktu--){
+                                    echo $waktu;
+                                    sleep(1);
+                                    $pjg = strlen($waktu);
+                                    for ($pjg; $pjg > 0; $pjg--){
+                                        echo chr(8);
+                                    }
+                                }
+                                if($openchest['status'] == 1){
+                                    echo " + ".$openchest['data']['num']."\n";
+                                }
+                                else{
+                                    echo $merah."Gagal Membuka Chest \n".$t;
+                                    $bykces = 0;
                                 }
                             }
-                            if($openchest['status'] == 1){
-                                echo " + ".$openchest['data']['num']."\n";
-                            }
-                            else{
-                                echo $merah."Gagal Membuka Chest \n".$t;
-                                $bykces = 0;
-                            }
                         }
+                    
                     }
                     else{
                         echo "-------------------------------------------------------\n";
@@ -197,7 +242,8 @@
                 }
             }
             if($pilihan == 2){
-                echo "Nomor 2 \n";
+                echo "Membuka Chest Dalam : ";
+                echo "\n";
             }
             if($pilihan == 3){
                 echo $bold.$merah."Keluar Dari Bot \n".$normal.$t;
