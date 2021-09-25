a4642f9d91ae54d9cbff6af2c67ec10b419a426f
Ryan Kurniadi <35515863+ryankrv@users.noreply.github.com>
2021-09-25 11:13:35 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,5 @@
 <?php 
         $uid = '99ED4697C480A58A';
         $token = 'By0DcAI8XT4NclE_VyZdbwI5CjhXOlUwAm9QOQQkXHNUJwUgUyYAPQpmXT8EZFNiVmEJNABtUj9TNVo4A3w';
-        $timestamp = 'timeStamp=1632486949&apiSign=1B3146C5CA33A5200502639D35CCDC4F';
      
 ?>
